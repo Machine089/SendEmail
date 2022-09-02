ab52c7e73e696d66054ff45bceb9ca0b7c34d116
Machine089 <Khizriev089@gmail.com>
2022-09-02 22:44:36 +0300

    Tc

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import os
 import smtplib
+import mimetypes
+from email import encoders
+from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
+from email.mime.audio import MIMEAudio
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
 
 class SendEmail:
 
@@ -14,8 +22,39 @@ class SendEmail:
 
         try:
             server.login(self.email_sender, self.password)
-            msg = MIMEText(message)
+            msg = MIMEMultipart()
             msg["subject"] = 'Topic'
+
+            if message:
+                msg.attach(MIMEText(message))
+
+            for file in os.listdir("attachments"):
+                filename = os.path.basename(file)
+                ftype, encoding = mimetypes.guess_type(file)
+                file_type, subtype = ftype.split("/")
+
+                if file_type == 'text':
+                    with open(f"attachments/{file}") as f:
+                        file = MIMEText(f.read())
+                elif file_type == 'image':
+                    with open(f"attachments/{file}", "rb") as f:
+                        file = MIMEImage(f.read(), subtype)
+                elif file_type == 'audio':
+                    with open(f"attachments/{file}", "rb") as f:
+                        file = MIMEAudio(f.read(), subtype)
+                elif file_type == 'application':
+                    with open(f"attachments/{file}", "rb") as f:
+                        file = MIMEApplication(f.read(), subtype)
+                else:
+                    with open(f"attachments/{file}", "rb") as f:
+                        file = MIMEBase(file_type, subtype)
+                        file.set_payload(f.read())
+                        encoders.encode_base64(file)
+
+                file.add_header('content-disposition', 'attachment', filename=filename)
+                msg.attach(file)
+
+
             server.sendmail(self.email_sender, self.email_recipient, msg.as_string())
             return "The message was sent succesfully"
         except Exception as _ex:
