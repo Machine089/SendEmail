5e610a87d1db514c022f8f25a61be95fccac2e80
Machine089 <Khizriev089@gmail.com>
2022-09-02 14:01:49 +0300

    Sc

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import smtplib
 from email.mime.text import MIMEText
 
 class SendEmail:
-    
+
     def __init__(self, email_sender, password, email_recipient):
         self.email_sender = email_sender
         self.password = password
@@ -22,11 +22,11 @@ class SendEmail:
             return f"{_ex}\nCheck your login or password"
 
 
-sender = "Enter the sender email"
-password = "Enter the sender password, if use gmail then enter application password"
-recipient = "Enter the recipien email"
+sender = input("Enter the sender email: ")
+password = input("Enter the sender password, if use gmail then enter application password: ")
+recipient = input("Enter the recipien email: ")
 
-msg = "Enter any text you want to send"
+msg = input("Enter any text you want to send: ")
 
 a = SendEmail(sender, password, recipient)
-a.send_message(msg)
+print(a.send_message(msg))
